b07e8ad85dab983e7fee007bcce3f635a06686f8
zHaytam <haytam.zanid@gmail.com>
2019-02-21 21:02:23 +0100

    Added "Modern UI" asset

---
+++ HeldenClient/Assets/Scripts/Screens/RegistrationScreen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Screens
+{
+    public class RegistrationScreen : MonoBehaviour
+    {
+
+        #region Fields
+
+        [SerializeField] private TMP_InputField _emailInput;
+        [SerializeField] private Button _registerBtn;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            Debug.Log("OnAwake");
+            _registerBtn.onClick.AddListener(RegisterBtnClicked);
+        }
+
+        private void RegisterBtnClicked()
+        {
+            Debug.Log(_emailInput.text);
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("OnDestroy");
+            _registerBtn.onClick.RemoveListener(RegisterBtnClicked);
+        }
+
+        #endregion
+
+    }
+}
